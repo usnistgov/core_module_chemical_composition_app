71585b6e810e3280a7a6665bc1fac195526dd97e
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:28 -0400

    fix: revert django upgrade
    Change-Id: I979db37944c4ad9b0263ba638917e65445df1a68

--- core_module_chemical_composition_app/urls.py
+++ core_module_chemical_composition_app/urls.py
@@ -1,12 +1,11 @@
 """ Url router for the chemical composition module
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_module_chemical_composition_app.views import ChemicalCompositionModule
 
 urlpatterns = [
-    re_path(r'module-chemical-composition',
-            ChemicalCompositionModule.as_view(),
-            name='core_module_chemical_composition'),
+    url(r'module-chemical-composition',
+        ChemicalCompositionModule.as_view(),
+        name='core_module_chemical_composition'),
 ]
