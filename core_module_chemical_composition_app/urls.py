ce11275b7910d8d7f4279671ef925d9e593f2c38
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-30 10:16:27 -0400

    feat: upgrade django 2.2
    Change-Id: Ie68c4b1a271504352aba2dad6aac6731fa8b4281

--- core_module_chemical_composition_app/urls.py
+++ core_module_chemical_composition_app/urls.py
@@ -1,11 +1,12 @@
 """ Url router for the chemical composition module
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_module_chemical_composition_app.views import ChemicalCompositionModule
 
 urlpatterns = [
-    url(r'module-chemical-composition',
-        ChemicalCompositionModule.as_view(),
-        name='core_module_chemical_composition'),
+    re_path(r'module-chemical-composition',
+            ChemicalCompositionModule.as_view(),
+            name='core_module_chemical_composition'),
 ]
